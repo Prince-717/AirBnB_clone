ad55ef7bd6c361d37f2d5cc06057ec00510ac74c
Prince Aboagye <aboagyeprince717@gmail.com>
2023-07-15 15:00:46 -0700

    Write amenity class that inherits from BaseModel

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Amenity class
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+        Args:
+        BaseModel ([class]): class that inherited by Amenity
+    """
+    name = ""
